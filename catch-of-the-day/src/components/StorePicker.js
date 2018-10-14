c2e0143c0cce12ea43d48a1d321857db3c1f8638
LifeH <hwie.zhu@gmail.com>
2018-10-14 19:24:02 -0400

    instead of constructor with method, opted for property set to arrow function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,39 @@ import React, { Fragment } from 'react';
 import { getFunName } from '../helpers';
 
 export default class StorePicker extends React.Component {
+  // handleClick(){
+  //   alert('hey');
+  // }
+
+  // constructor(){
+  //   super();
+  //   this.goToStore=this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  }
+
+  // componentDidMount() {
+  //   console.log("Mounted");
+  //   console.log(this);
+  // }
+
   render() {
+    // console.log(this);
     return (
       <Fragment>
-        <form className='store-selector'>
+        <form className='store-selector' onSubmit={this.goToStore}>
           <h2> Enter a store</h2>
+          {/* <button onClick={this.handleClick}> Click Me!</button> */}
           <p>I am the store picker!</p>
           <input
-            type="text"
-            placeholder="Store Name"
+            type="text" 
+            ref={this.myInput}
+            required placeholder="Store Name"
             defaultValue={getFunName()} />
           <button type="submit">Visit Store</button>
         </form>
