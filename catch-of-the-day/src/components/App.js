b03ced311b8800cc481977b7abe2a9aeb8590572
LifeH <hwie.zhu@gmail.com>
2018-10-14 21:52:17 -0400

    added form to populate fish, form reset, setstate on app that passes down to Inventory that passes down to AddFishForm

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 export default class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    //1. Take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    //2. Add our new fish to that fishes cariable
+    fishes[`fish${Date.now()}`] = fish;
+    //fish goes down to addFishForm in the props.fish
+    //3. set the new fishes object to state
+    this.setState({
+      fishes
+    });
+    console.log("fish");
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" age={100}></Header>
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
